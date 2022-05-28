83f8ec13fa4032b08b16ba06d15c24aaf78131a9
phungSY236 <phungvansyhb@gmail.com>
2022-05-28 17:13:07 +0700

    AIRSENCE-40 : fix name component

---
+++ pages/register.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import FormRegister from "../src/components/common/FormRegister";
+
+function Register(props) {
+    return (
+        <FormRegister bgImage={"/bg2.jpg"} routePass={"/login"} />
+    )
+}
+
+export default Register;
